d2332cb2d73833ed981859b7345edf548e692401
Switch <thunyoubun@gmail.com>
2023-04-09 22:58:04 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!" , intents=discord.Intents.all())
 
-BOT_TOKEN = "MTAxNDkxOTc5OTA4NTc0NDI1OQ.G-UWbS.yykAgMjb2-WaYGUycmcVHU7ZcRhiYdCZbeCnMs"
+BOT_TOKEN = "TOKEN"
 
 
 class Bot(commands.Bot):
